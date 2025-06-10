83efe994c2e9cfb4a3427a395219e4b96e86c22a
it-21@1bzimail.com <it-21@1bzimial.com>
2025-06-10 17:07:52 +0800

    更新

--- static/base/navigation/notification/notification.js
+++ static/base/navigation/notification/notification.js
@@ -2,28 +2,25 @@
 // message 消息
 // notification-referer @我
 
-const notificationContentLabel = document.getElementById(
-  "notification-content"
-);
+const DrawerNotificationBut  = document.getElementById("DrawerNotificationBut") // 触发消息按钮
+const NotificationContentLabel = document.getElementById("notification-content"); // 消息内容
 
 // ajax请求，发送后端请求数据
 function notificationMessage(params) {
   // params：点击时的按钮id
   console.log(params);
   setTimeout(() => {
-    notificationContentLabel.innerHTML = `<s-empty>暂时没有消息</s-empty>`;
+    NotificationContentLabel.innerHTML = `<s-empty>暂时没有消息</s-empty>`;
   }, 5000);
 }
 
 // 对通知设置点击事件
 function createNotificationClick(notificationId) {
   const el = document.getElementById(notificationId);
-  
   if (!el) return;
   el.addEventListener("click", () => {
-    notificationContentLabel.innerHTML = `<s-circular-progress indeterminate="true" class="notification-content-circular-progress"></s-circular-progress>`;
+    NotificationContentLabel.innerHTML = `<s-circular-progress indeterminate="true" class="notification-content-circular-progress"></s-circular-progress>`;
     notificationMessage(notificationId);
-    
   });
 }
 
@@ -37,3 +34,7 @@ const menuNList = [
 for (const id of menuNList) {
     createNotificationClick(id);
 }
+
+DrawerNotificationBut.addEventListener("click",()=>{
+
+})
