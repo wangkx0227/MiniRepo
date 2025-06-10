28cef17ad48fdd989a8db68a13d63082c44f0991
it-21@1bzimail.com <it-21@1bzimial.com>
2025-06-10 17:13:51 +0800

    更新通知选项卡触发函数效果与点击事项设置

--- static/base/navigation/notification/notification.js
+++ static/base/navigation/notification/notification.js
@@ -6,7 +6,7 @@ const DrawerNotificationBut  = document.getElementById("DrawerNotificationBut") 
 const NotificationContentLabel = document.getElementById("notification-content"); // 消息内容
 
 // ajax请求，发送后端请求数据
-function notificationMessage(params) {
+function notificationMessageAjax(params) {
   // params：点击时的按钮id
   console.log(params);
   setTimeout(() => {
@@ -14,27 +14,34 @@ function notificationMessage(params) {
   }, 5000);
 }
 
-// 对通知设置点击事件
+// 对通知选项卡设置点击事件
 function createNotificationClick(notificationId) {
   const el = document.getElementById(notificationId);
   if (!el) return;
   el.addEventListener("click", () => {
     NotificationContentLabel.innerHTML = `<s-circular-progress indeterminate="true" class="notification-content-circular-progress"></s-circular-progress>`;
-    notificationMessage(notificationId);
+    notificationMessageAjax(notificationId);
   });
 }
 
-const menuNList = [
+const tabNList = [
   "notification-referer",
   "notification-message",
   "notification-other",
 ];
 
 // 循环生成或者说赋值点击函数
-for (const id of menuNList) {
+for (const id of tabNList) {
     createNotificationClick(id);
 }
 
+// 当点击触发消息按钮进行加载-@我的内容
+let notificationPopupLoaded = false; // 防止多次加载
 DrawerNotificationBut.addEventListener("click",()=>{
-
+  if (!notificationPopupLoaded) {
+    // 自动触发第一个 tab 的加载
+    NotificationContentLabel.innerHTML = `<s-circular-progress indeterminate="true" class="notification-content-circular-progress"></s-circular-progress>`;
+    notificationMessageAjax("notification-referer"); // 假设这是加载函数
+    notificationPopupLoaded = true;
+  }
 })
