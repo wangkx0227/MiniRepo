f704dcc6293dafc27071d15ca6757953902fea9d
wangkx0227@163.com <wangkx0227@163.com>
2025-06-12 08:30:36 +0800

    更新用户部分视图

--- blueprints/authentication/views.py
+++ blueprints/authentication/views.py
@@ -2,24 +2,24 @@ from flask import render_template
 
 
 def home():
-    return render_template("user/home.html")
+    return render_template("home.html")
 
 
 def setting():
-    return render_template("user/setting.html")
+    return render_template("setting.html")
 
 
 def todos():
-    return render_template("user/todos.html")
+    return render_template("todos.html")
 
 
 def related():
-    return render_template("user/related.html")
+    return render_template("related.html")
 
 
 def stars():
-    return render_template("user/stars.html")
+    return render_template("stars.html")
 
 
 def repos():
-    return render_template("user/repos.html")
+    return render_template("repos.html")
