f6a67ec7d2e923eec722ad7cab94763b8600adac
wangkx0227@163.com <wangkx0227@163.com>
2025-06-12 11:21:07 +0800

    更新首页

--- core/app.py
+++ core/app.py
@@ -3,8 +3,11 @@ from blueprints import user_bp, dashboard_bp, new_bp
 
 
 def create_app():
-    app = Flask(__name__, template_folder='../templates', static_folder='../static')
+    app = Flask(__name__, template_folder='../templates',
+                static_folder='../static')
     app.register_blueprint(new_bp)
     app.register_blueprint(user_bp)
     app.register_blueprint(dashboard_bp)
     return app
+
+
--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+from flask import redirect, url_for
 from core import create_app
 
+
 app = create_app()
+
+
+@app.route("/")
+def index():
+    return redirect(url_for("dashboard.workbenches"))
+
+
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, port=5000, host="127.0.0.1")
